86329f63ad9c8b9acd034b1618ee0efc73e8eb28
Daniel Ochoja <danielochoja@yahoo.com>
2024-03-05 03:48:47 +0100

    added upload functionality, completed utils function

--- api/app.py
+++ api/app.py
@@ -1,11 +1,59 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
+import os
+import json
+from api.utils import Utils
 
 app = Flask(__name__)
 CORS(app)
 
+UPLOAD_FOLDER = './uploads/'
+ALLOWED_EXTENSIONS = {'csv'}
 
-@app.route("/api/check_probability", methods=['GET', 'POST'])
-def check_probability():
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+Utils = Utils()
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/api/benford_test/upload/", methods=['GET', 'POST'])
+def benford_test_file():
     """Get expected and actual values"""
-    return jsonify()
+    if 'file' not in request.files:
+        return 'No file part'
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return 'No selected file'
+
+    if file and allowed_file(file.filename):
+        filename = file.filename
+        print(filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return 'File uploaded successfully'
+    else:
+        return jsonify({'error': 'Invalid file type'}), 400
+
+
+@app.route("/api/benford_test/")
+def benford_test():
+    data = request.args.get('data')
+
+    try:
+        data = json.loads(data)
+        if type(data) is not list:
+            raise Exception
+        else:
+            values = Utils.extract_first_digits(data)
+            actual_percentages = Utils.get_digit_percentages(values)
+            expected_percentages = Utils.get_expected_percentages()
+            p_value = Utils.get_p_value(values)
+    except Exception:
+        return jsonify({'error': 'Invalid parameter or format'}), 400
+
+    return jsonify({'actual_percentages': actual_percentages, 'expected_percentages': expected_percentages, 'p-value': p_value})
--- api/utils.py
+++ api/utils.py
@@ -1,9 +1,10 @@
 """Contains utilities for calculating benford Law"""
 import pandas as pd
+from scipy.stats import chisquare
 
 
 class Utils:
-    """Checks the quality of data"""
+    """Benford's law Utility Functions"""
 
     def check_dataSet(self, data: list[str | int | float]) -> bool:
         """Check if data contains numbers only"""
@@ -20,7 +21,8 @@ class Utils:
         return True  # return true if all items pass test
 
     def extract_csv_values(self, filename: str, column: str) -> list:
-        """Extract values from a column"""
+        """Extract values from a column
+            Save only number values"""
         try:
             df = pd.read_csv(filename)  # loads file into dataframe
             values = pd.to_numeric(df[column], errors='coerce')
@@ -42,7 +44,8 @@ class Utils:
         return first_digits
 
     def count_digits(self, data: list[int]) -> dict:
-        """Count number of digits from 1-9"""
+        """Count number of occurences
+            of digits from 1-9"""
         digit_counts = {i: 0 for i in range(1, 10)}
         for number in data:
             if number in digit_counts:
@@ -58,6 +61,30 @@ class Utils:
         for digit in digit_counts.keys():
             if digit_counts[digit]:
                 digit_percentages[digit] = round((
-                    digit_counts[digit] / total_digits) * 100, 2)
+                    digit_counts[digit] / total_digits) * 100, 3)
 
         return digit_percentages
+
+    def get_expected_percentages(self) -> dict:
+        """Get Expected percentage based on Benford Law"""
+        return {1: 30.1, 2: 17.6, 3: 12.5, 4: 9.7, 5: 7.9, 6: 6.7, 7: 5.8, 8: 5.1, 9: 4.6}
+
+    def get_p_value(self, data: list[int]) -> float:
+        """Get p-value and perform chi-square test"""
+        expected_percentages = [30.1, 17.6, 12.5,
+                                9.7, 7.9, 6.7, 5.8, 5.1, 4.6]
+        observed_percentages = self.get_digit_percentages(data)
+
+        total_expected_percentage = sum(expected_percentages)
+        total_observed_percentage = sum(observed_percentages.values())
+
+        # Adjust observed percentages to match the total sum of expected percentages
+        for digit in observed_percentages:
+            observed_percentages[digit] *= total_expected_percentage / \
+                total_observed_percentage
+
+        # perform the chi-square test
+        chi2_stat, p_value = chisquare(
+            list(observed_percentages.values()), f_exp=expected_percentages)
+
+        return p_value
