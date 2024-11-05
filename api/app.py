c35be95006149cfa5e8911faefafe711e19972a4
Joshua <68911373+GEPaDC@users.noreply.github.com>
2024-11-05 01:02:45 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,3 +1,23 @@
+from flask import Flask, jsonify, request, abort
+from flask_cors import CORS
+import os
+import json
+from api.utils import Utils
+
+app = Flask(__name__)
+CORS(app)
+
+UPLOAD_FOLDER = './'
+ALLOWED_EXTENSIONS = {'csv', 'xls', 'xlsx'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+Utils = Utils()  # Instantiate the Utils class
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route("/api/benford_test/")
 def benford_test():
     data = request.args.get('data')
@@ -6,6 +26,7 @@ def benford_test():
         return jsonify({'error': 'Missing data parameter', 'details': 'The data parameter is required and must be a JSON array'}), 400
 
     try:
+        # Parse the data as a JSON array
         data = json.loads(data)
         if not isinstance(data, list):
             raise ValueError("Data is not a list")
@@ -35,3 +56,6 @@ def benford_test():
         })
     except Exception as e:
         return jsonify({'error': 'Invalid parameter or format', 'details': str(e)}), 400
+
+if __name__ == "__main__":
+    app.run(debug=True)
